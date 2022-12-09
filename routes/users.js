c0faf0049a6c5cd744ace072b895e3f7eace4968
maxstark <max@max-stark.com>
2022-12-09 13:08:44 +0530

    First Commit

---
+++ routes/users.js
@@ -0,0 +1,66 @@
+import  express  from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
+
+const router = express.Router();
+
+let users = [];
+
+router.get('/', (req,res) => {
+  console.log(users);
+  res.send(users);
+});
+
+router.post('/', (req,res) => {
+  console.log("POST ROUTE REACHED");
+
+  const user=req.body;
+
+  const userId= uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+
+  const userWithId = {...user, id: userId};
+
+  users.push(userWithId);
+
+  res.send(`User with the name ${user.firstName} added to the database!`);
+})
+
+router.get('/:id', (req,res) => {
+
+  const {id} = req.params;
+  
+  const foundUser = users.find((user)=> user.id === id);
+  res.send(foundUser);
+})
+
+router.delete('/:id', (req,res) =>{
+  const { id } = req.params;
+
+  users = users.filter((user)=> user.id !== id);
+
+  res.send(`User with id ${id} is deleted`);
+
+})
+
+router.patch('/:id',(req,res)=>
+{
+  const {id} = req.params;
+  const {firstName, lastName, age} = req.body;
+  
+  const user = users.find((user)=> user.id === id);
+  
+  if(lastName){
+    user.lastName= lastName;
+  }
+  if(firstName){
+    user.firstName=firstName;
+  }
+  if(age){
+    user.age = age;
+  }
+
+  res.send('User with id has been updated');
+
+})
+
+export default router;
